3acf98c8192287d878be226151272c4d66d62dec
Vomitatrix <david.j.procop@gmail.com>
2022-04-25 00:56:46 -0400

    Added breakdown for gross and net incomes

--- tax-calculator/script.js
+++ tax-calculator/script.js
@@ -1,7 +1,20 @@
 const incomeButton = document.querySelector('button');
-const yearlyIncomeDisplay = document.getElementById('yearlyIncome');
+
 const taxDueDisplay = document.getElementById('taxDue');
-const netIncomeDisplay = document.getElementById('netIncome');
+
+const grossYearlyDisplay = document.getElementById('grossYearly');
+const grossMonthlyDisplay = document.getElementById('grossMonthly');
+const grossBiweeklyDisplay = document.getElementById('grossBiweekly');
+const grossWeeklyDisplay = document.getElementById('grossWeekly');
+const grossDailyDisplay = document.getElementById('grossDaily');
+const grossHourlyDisplay = document.getElementById('grossHourly');
+
+const netYearlyDisplay = document.getElementById('netYearly');
+const netMonthlyDisplay = document.getElementById('netMonthly');
+const netBiweeklyDisplay = document.getElementById('netBiweekly');
+const netWeeklyDisplay = document.getElementById('netWeekly');
+const netDailyDisplay = document.getElementById('netDaily');
+const netHourlyDisplay = document.getElementById('netHourly');
 
 const TAX10 = 1027.5;
 const TAX12 = 4807.5;
@@ -10,34 +23,68 @@ const TAX24 = 34647.5;
 const TAX32 = 49335.5;
 const TAX35 = 162718;
 
-let yearlyIncome;
 let taxDue;
-let netIncome;
+
+let grossYearly;
+let grossMonthly;
+let grossBiweekly;
+let grossWeekly;
+let grossDaily;
+let grossHourly;
+let netYearly;
+let netMonthly;
+let netBiweekly;
+let netWeekly;
+let netDaily;
+let netHourly;
 
 incomeButton.addEventListener('click', calculateTax);
 
 function calculateTax() {
-    yearlyIncome = prompt('Input yearly income:');
+    grossYearly = prompt('Input gross yearly income:');
 
-    if (yearlyIncome > 539900) {
-        taxDue = ((yearlyIncome - 539900)*0.37) + TAX35;
-    } else if (yearlyIncome > 215950) {
-        taxDue = ((yearlyIncome - 215950) * 0.35) + TAX32;
-    } else if (yearlyIncome > 170050) {
-        taxDue = ((yearlyIncome - 170050) * 0.32) + TAX24;
-    } else if (yearlyIncome > 89075) {
-        taxDue = ((yearlyIncome - 89075) * 0.24) + TAX22;
-    } else if (yearlyIncome > 41775) {
-        taxDue = ((yearlyIncome - 41775) * 0.22) + TAX12;
-    } else if (yearlyIncome > 10275) {
-        taxDue = ((yearlyIncome - 10275) * 0.12) + TAX10;
+    if (grossYearly > 539900) {
+        taxDue = ((grossYearly - 539900)*0.37) + TAX35;
+    } else if (grossYearly > 215950) {
+        taxDue = ((grossYearly - 215950) * 0.35) + TAX32;
+    } else if (grossYearly > 170050) {
+        taxDue = ((grossYearly - 170050) * 0.32) + TAX24;
+    } else if (grossYearly > 89075) {
+        taxDue = ((grossYearly - 89075) * 0.24) + TAX22;
+    } else if (grossYearly > 41775) {
+        taxDue = ((grossYearly - 41775) * 0.22) + TAX12;
+    } else if (grossYearly > 10275) {
+        taxDue = ((grossYearly - 10275) * 0.12) + TAX10;
     } else {
-        taxDue = yearlyIncome * 0.1;
+        taxDue = grossYearly * 0.1;
     }
 
-    netIncome = yearlyIncome - taxDue;
-    
-    yearlyIncomeDisplay.textContent = Number(yearlyIncome).toLocaleString('en-US');
+    grossMonthly = (grossYearly / 12).toFixed(2);
+    grossHourly = (grossYearly / 2080).toFixed(2);
+    grossDaily = grossHourly * 8;
+    grossWeekly = grossDaily * 5;
+    grossBiweekly = grossWeekly * 2;
+
+    netYearly = grossYearly - taxDue;
+    netMonthly = (netYearly / 12).toFixed(2);
+    netHourly = (netYearly / 2080).toFixed(2);
+    netDaily = netHourly * 8;
+    netWeekly = netDaily * 5;
+    netBiweekly = netWeekly * 2;
+
     taxDueDisplay.textContent = Number(taxDue).toLocaleString('en-US');
-    netIncomeDisplay.textContent = Number(netIncome).toLocaleString('en-US');
+
+    grossYearlyDisplay.textContent = Number(grossYearly).toLocaleString('en-US');
+    grossMonthlyDisplay.textContent = Number(grossMonthly).toLocaleString('en-US');
+    grossBiweeklyDisplay.textContent = Number(grossBiweekly).toLocaleString('en-US');
+    grossWeeklyDisplay.textContent = Number(grossWeekly).toLocaleString('en-US');
+    grossDailyDisplay.textContent = Number(grossDaily).toLocaleString('en-US');
+    grossHourlyDisplay.textContent = Number(grossHourly).toLocaleString('en-US');
+
+    netYearlyDisplay.textContent = Number(netYearly).toLocaleString('en-US');
+    netMonthlyDisplay.textContent = Number(netMonthly).toLocaleString('en-US');
+    netBiweeklyDisplay.textContent = Number(netBiweekly).toLocaleString('en-US');
+    netWeeklyDisplay.textContent = Number(netWeekly).toLocaleString('en-US');
+    netDailyDisplay.textContent = Number(netDaily).toLocaleString('en-US');
+    netHourlyDisplay.textContent = Number(netHourly).toLocaleString('en-US');
 }
